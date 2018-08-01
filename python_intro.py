ce7d7512352f926f8996ccaa33efb62d3f51311a
Sanggee Cho <chosg@kois.co.kr>
2018-08-01 13:14:51 +0900

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,32 @@
+# i = 10
+# if i > 10:
+#     print("Hello, Django Girls.")
+# elif i > 0:
+#     print("true")
+# else :
+#     print("False")
+
+def hi(name):
+    print('Hi ' + name + '~!')
+#     print('How are you?')
+# hi()    
+# hi()    
+#     if name == 'Ola':
+#         print('Hi ' + name)
+#     elif name == 'Sonja':
+#         print('Hi ' + name)
+#     else:
+#         print('No Name')
+# hi('Ola')
+# hi('')
+# girls = ['a', 'b', 'c', 'd']
+# print(girls)
+# for girl in girls:
+#     hi(girl)
+# for girl in girls:
+#     print('Hi ' + girl + '!')    
+
+# for i in [1,2,3,4,5]    :
+#     print(i * 2)
+for i in range(1, 6):
+    print(i * 2)
